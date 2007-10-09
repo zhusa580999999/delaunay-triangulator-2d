0aac6454bcf46e3bc6dc3bcd5f11c048d972a711
mario@tanev.net <mario@tanev.net@d4e2ac63-e33b-0410-830a-27f7ff4079df>
2007-10-09 06:53:44 +0000

    Add unit-testing infrastructure.

---
+++ trunk/src/testing/testing_main.cc
@@ -0,0 +1,28 @@
+// Main function to link in for creating unit-tests.
+
+#include <cppunit/TestRunner.h>
+#include <cppunit/TestResult.h>
+#include <cppunit/TestResultCollector.h>
+#include <cppunit/BriefTestProgressListener.h>
+#include <cppunit/extensions/TestFactoryRegistry.h>
+
+int main(int argc, char** argv)
+{
+  // Create the event manager and test controller
+  CPPUNIT_NS::TestResult controller;
+
+  // Add a listener that collects test result
+  CPPUNIT_NS::TestResultCollector result;
+  controller.addListener(&result);        
+
+  // Add a listener that prints dots as tests run
+  CPPUNIT_NS::BriefTestProgressListener progress;
+  controller.addListener(&progress);      
+
+  // Add the top suite to the test runner
+  CPPUNIT_NS::TestRunner runner;
+  runner.addTest(CPPUNIT_NS::TestFactoryRegistry::getRegistry().makeTest());
+  runner.run(controller);
+
+  return result.wasSuccessful() ? 0 : 1;
+}
