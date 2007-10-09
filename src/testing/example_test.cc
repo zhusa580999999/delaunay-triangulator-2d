cb820bb753c7f5209f54f7766147c3a3239decfa
mario <mario@tanev.net>
2007-10-09 06:53:44 +0000

    Add unit-testing infrastructure.

---
+++ src/testing/example_test.cc
@@ -0,0 +1,21 @@
+// An example implementation of a test suite.
+
+#include <cppunit/TestCase.h>
+#include <cppunit/extensions/HelperMacros.h>
+
+class ExampleTest : public CPPUNIT_NS::TestCase {
+  CPPUNIT_TEST_SUITE(ExampleTest);
+  CPPUNIT_TEST(testAddition);
+  CPPUNIT_TEST_SUITE_END();
+
+ public:
+  void setUp(void) {}
+  void tearDown(void) {} 
+
+ protected:
+  void testAddition() {
+   CPPUNIT_ASSERT(5 == 2 + 3);
+  }
+};
+
+CPPUNIT_TEST_SUITE_REGISTRATION(ExampleTest);
