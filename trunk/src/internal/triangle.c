611dfe6b1cd21fba7f3ba71b255b1295f851df2d
mario@tanev.net <mario@tanev.net@d4e2ac63-e33b-0410-830a-27f7ff4079df>
2007-10-09 05:48:24 +0000

    Add CMakeLists.txt and restructure tree.
    Also, now triangle can compile both as a library and a binary.
    
    Some documentation has been added:
    BUILDING - describes the building process
    LAYOUT - describes the file tree

--- trunk/src/internal/triangle.c
+++ trunk/src/internal/triangle.c
@@ -354,7 +354,7 @@
 #include <fpu_control.h>
 #endif /* LINUX */
 #ifdef TRILIBRARY
-#include "triangle.h"
+#include "public/triangle.h"
 #endif /* TRILIBRARY */
 
 /* A few forward declarations.                                               */
